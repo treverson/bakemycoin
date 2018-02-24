7ad600ca1416968ea24c20d9ffa90aa49a5a3b80
root <root@localhost.localdomain>
2018-02-24 18:10:49 +0200

    auto python and packages downloader

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+sudo apt-get update
+sudo apt-get --assume-yes install build-essential
+sudo apt-get --assume-yes install  libreadline-gplv2-dev libncursesw5-dev libss$
+apt-get --assume-yes  install python
+apt-get --assume-yes  install python-requests
